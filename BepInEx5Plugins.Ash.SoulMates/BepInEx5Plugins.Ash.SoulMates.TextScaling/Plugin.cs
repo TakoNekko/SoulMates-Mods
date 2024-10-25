57143c42c57dc46a71d45cf7557a1d9a22983bdc
ash <TakoNekko@users.noreply.github.com>
2024-10-25 20:11:59 +0700

    Add project for text scaling mod

---
+++ BepInEx5Plugins.Ash.SoulMates/BepInEx5Plugins.Ash.SoulMates.TextScaling/Plugin.cs
@@ -0,0 +1,30 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+
+namespace BepInEx5Plugins.Ash.SoulMates.TextScaling
+{
+	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+	public class Plugin : BaseUnityPlugin
+	{
+		private Plugin()
+		{
+		}
+
+		private void Awake()
+		{
+			try
+			{
+				Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+				var harmony = new Harmony(Info.Metadata.GUID);
+
+				harmony.PatchAll();
+			}
+			catch (Exception exception)
+			{
+				Console.WriteLine(exception);
+			}
+		}
+	}
+}
